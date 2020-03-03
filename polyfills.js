4f583efc773b967550c606527f35108b07b18db9
powerfulyang <i@powerfulyang.com>
2020-03-03 19:09:40 +0800

    :seedling::seedling::seedling::seedling::seedling:

---
+++ polyfills.js
@@ -0,0 +1,16 @@
+// /core/polyfills.js
+/* eslint no-extend-native: 0 */
+// core-js comes with Next.js. So, you can import it like below
+import includes from 'core-js/es/string/virtual/includes';
+import repeat from 'core-js/es/string/virtual/repeat';
+import assign from 'core-js/es/object/assign';
+import 'core-js/es/map';
+import 'core-js/es/set';
+
+// Add your polyfills
+// This files runs at the very beginning (even before React and Next.js core)
+console.log('Load your polyfills');
+
+String.prototype.includes = includes;
+String.prototype.repeat = repeat;
+Object.assign = assign;
---
+++ src/components/BackGround/fx_brightbuf_fsh.glsl
@@ -0,0 +1,14 @@
+#ifdef GL_ES
+//precision mediump float;
+precision highp float;
+#endif
+uniform sampler2D uSrc;
+uniform vec2 uDelta;
+
+varying vec2 texCoord;
+varying vec2 screenCoord;
+
+void main(void) {
+    vec4 col = texture2D(uSrc, texCoord);
+    gl_FragColor = vec4(col.rgb * 2.0 - vec3(0.5), 1.0);
+}
