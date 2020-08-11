9cbfd3d02ad1bdfc339de37058de973a663e3e4b
powerfulyang <i@powerfulyang.com>
2020-08-12 01:46:11 +0800

    refactor(panel): todo panel page

--- next.config.js
+++ next.config.js
@@ -1,37 +1,33 @@
 const withSass = require('@zeit/next-sass');
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass({
-    webpack (config, options) {
-        config.module.rules.push({
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000
-                }
-            }
-        });
-        config.module.rules.push({
-            test: /\.(glsl|vs|fs|vert|frag)$/,
-            exclude: /node_modules/,
-            use: [
-                'raw-loader',
-                'glslify-loader'
-            ]
-        });
-        const originalEntry = config.entry;
-        config.entry = async () => {
-            const entries = await originalEntry();
+const withCSS = require('@zeit/next-css');
+module.exports = withCSS(
+  withSass({
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+          },
+        },
+      });
+      config.module.rules.push({
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader', 'glslify-loader'],
+      });
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = await originalEntry();
 
-            if (
-                entries['main.js'] &&
-                !entries['main.js'].includes('./polyfills.js')
-            ) {
-                entries['main.js'].unshift('./polyfills.js')
-            }
+        if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
+          entries['main.js'].unshift('./polyfills.js');
+        }
 
-            return entries
-        };
-        return config;
-    }
-}));
+        return entries;
+      };
+      return config;
+    },
+  }),
+);
