7340e0b5085b474c4fcb19bfa7dae68db17819f1
powerfulyang <i@powerfulyang.com>
2020-09-06 21:43:45 +0800

    chore(blog): rewrite blog address

--- src/Config/config.tsx
+++ src/Config/config.tsx
@@ -14,11 +14,9 @@ export const ICP_NO = '粤ICP备19128686号-1';
 
 export const panelList = [
   { name: 'home', url: 'https://powerfulyang.com' },
-  { name: 'blog', url: 'https://blog.powerfulyang.com' },
-  { name: 'book', url: 'https://book.powerfulyang.com' },
+  { name: 'blog', url: 'https://medium.com/@hutyxxx' },
   { name: 'gallery', url: 'https://gallery.powerfulyang.com' },
   { name: 'components', url: 'https://components.powerfulyang.com' },
-  { name: 'logs', url: 'https://logs.powerfulyang.com' },
   { name: 'monitor', url: 'https://monitor.powerfulyang.com' },
   { name: 'admin', url: 'https://admin.powerfulyang.com' },
 ];
