3ca48ae1723fa10b9ddaa853573501c86d4c8a51
powerfulyang <i@powerfulyang.com>
2020-10-07 12:04:19 +0800

    revert: revert MapleLeafFalling back

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 const withSass = require('@zeit/next-sass');
-module.exports =
-  withSass({
-    webpack(config, _options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-          },
+module.exports = withSass({
+  webpack(config, _options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
         },
-      });
-      config.module.rules.push({
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ['raw-loader', 'glslify-loader'],
-      });
-      const originalEntry = config.entry;
-      config.entry = async () => {
-        const entries = await originalEntry();
+      },
+    });
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
 
-        if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
-          entries['main.js'].unshift('./polyfills.js');
-        }
+      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
+        entries['main.js'].unshift('./polyfills.js');
+      }
 
-        return entries;
-      };
-      return config;
-    },
-  })
+      return entries;
+    };
+    return config;
+  },
+  cssModules: true,
+});
