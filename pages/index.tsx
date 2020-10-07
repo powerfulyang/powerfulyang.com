f146f71f0b377c863ae2f7f53541defee3b32223
powerfulyang <i@powerfulyang.com>
2020-10-07 12:04:19 +0800

    revert: revert MapleLeafFalling back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Header } from '@/components/Head';
+import MapleLeafFalling from '@/components/MapleLeafFalling';
+import { isSupportWebGL } from '@powerfulyang/utils';
+import { useMount } from '@powerfulyang/hooks';
+import classNames from 'classnames';
+import styles from './index.scss';
 
 type IndexProps = {};
 export const Index: FC<IndexProps> = () => {
+  const [supportWebGL, setState] = useState(false);
+  useMount(() => {
+    setState(isSupportWebGL());
+  });
   return (
     <>
       <Header />
-      <div className="bg-gray-300">Hello World!</div>
+      {supportWebGL && <MapleLeafFalling />}
+      <div className={classNames({ [styles.bg]: !supportWebGL })}>
+        <div className="w-2/3">Hello World!!!</div>
+      </div>
     </>
   );
 };
