89dd8aeda302d5e8fd5b45e050a46ebd4f045d31
powerfulyang <i@powerfulyang.com>
2020-11-20 18:54:27 +0800

    refactor(:book:): 重构markdown组件

--- next.config.js
+++ next.config.js
@@ -1,34 +1,29 @@
 const withSass = require('@zeit/next-sass');
-const withCss = require('@zeit/next-css');
 
-module.exports = withCss(
-  withSass({
-    webpack(config, _options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-          },
+module.exports = withSass({
+  webpack(config, _options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
         },
-      });
-      config.module.rules.push({
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        exclude: /node_modules/,
-        use: ['raw-loader', 'glslify-loader'],
-      });
-      const originalEntry = config.entry;
-      config.entry = async () => {
-        const entries = await originalEntry();
-
-        if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
-          entries['main.js'].unshift('./polyfills.js');
-        }
-
-        return entries;
-      };
-      return config;
-    },
-  }),
-);
+      },
+    });
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+      if (entries['main.js'] && !entries['main.js'].includes('./polyfills.js')) {
+        entries['main.js'].unshift('./polyfills.js');
+      }
+      return entries;
+    };
+    return config;
+  },
+});
