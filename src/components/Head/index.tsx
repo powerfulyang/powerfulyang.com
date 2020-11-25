171ccbd0f539f3bf566900470efc529d6bd8f344
powerfulyang <i@powerfulyang.com>
2020-11-26 01:58:00 +0800

    chore(:rabbit:): google analytics

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -13,6 +13,17 @@ export const Header: FC<HeaderProps> = ({ title }) => {
       <Head>
         <title>{title || 'powerfulyang'}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y0JKGR6P0S" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              'window.dataLayer = window.dataLayer || [];\n' +
+              '  function gtag(){dataLayer.push(arguments);}\n' +
+              "  gtag('js', new Date());\n" +
+              '\n' +
+              "  gtag('config', 'G-Y0JKGR6P0S');",
+          }}
+        />
       </Head>
       <TwitterFavoriteWithNoSSR />
     </>
