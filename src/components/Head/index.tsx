40f4ff7620a742ee039f1e5c3333601ab5267475
powerfulyang <i@powerfulyang.com>
2020-11-28 21:49:26 +0800

    fix: google analytics only use in prod mode, and prepare to test react hooks

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import Head from 'next/head';
 import './index.scss';
 import { TwitterFavoriteWithNoSSR } from '@/components/dynamic';
+import { __prod__ } from '@powerfulyang/utils';
 
 export interface HeaderProps {
   title?: string;
@@ -12,19 +13,23 @@ export const Header: FC<HeaderProps> = ({ title }) => {
     <>
       <Head>
         <title>{title || 'powerfulyang home page'}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y0JKGR6P0S" />
-        <script
-          /* eslint-disable-next-line react/no-danger */
-          dangerouslySetInnerHTML={{
-            __html:
-              'window.dataLayer = window.dataLayer || [];\n' +
-              '  function gtag(){dataLayer.push(arguments);}\n' +
-              "  gtag('js', new Date());\n" +
-              '\n' +
-              "  gtag('config', 'G-Y0JKGR6P0S');",
-          }}
-        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, maximum-scale=1.0" />
+        {__prod__ && (
+          <>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y0JKGR6P0S" />
+            <script
+              /* eslint-disable-next-line react/no-danger */
+              dangerouslySetInnerHTML={{
+                __html:
+                  'window.dataLayer = window.dataLayer || [];\n' +
+                  '  function gtag(){dataLayer.push(arguments);}\n' +
+                  "  gtag('js', new Date());\n" +
+                  '\n' +
+                  "  gtag('config', 'G-Y0JKGR6P0S');",
+              }}
+            />
+          </>
+        )}
       </Head>
       <TwitterFavoriteWithNoSSR />
     </>
