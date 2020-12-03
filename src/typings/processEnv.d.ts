bf0d95808ac43785d72f2f91d6824d094ba1c72d
powerfulyang <i@powerfulyang.com>
2020-12-03 23:47:13 +0800

    fix: home page request api url

---
+++ src/typings/processEnv.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    JWT_SECRET: string;
+    BASE_URL;
+  }
+}
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 export const request = async (url: string, options: RequestOptions) => {
   const { method = 'GET', ctx, pathVariable } = options;
-  const baseUrl = 'http://localhost:3001/api/';
+  const baseUrl = process.env.BASE_URL;
   let path = '';
   if (pathVariable) {
     path = pathVariable(ctx!);
