45853aa70f1bdb2c74bf04271740c8db834d2583
powerfulyang <i@powerfulyang.com>
2020-12-12 15:04:53 +0800

    fix: canvas drawImage order error.

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -14,7 +14,7 @@ const Posts: FC<PostProps> = ({ data }) => {
   const { tags, pathViewCount, content, user, createAt } = data;
   const postInfo = `post=>${user.nickname}|${DateFormat(createAt)}|${
     content.length
-  }|${pathViewCount}  \r\n\r\n`;
+  }|${pathViewCount}|${user.avatar}  \r\n\r\n`;
   const tagsInfo = `tags=>${(tags || []).join('|')}  \r\n\r\n`;
   const contents = content.replace(/(\r\n|\n)/, `\r\n\r\n${postInfo}${tagsInfo}`);
   return (
