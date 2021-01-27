d2a0a55a89b28673e5f5631ee8a09e0b1656e158
powerfulyang <i@powerfulyang.com>
2021-01-27 09:52:42 +0800

    perf: page commit

--- pages/example/CommitLog/index.tsx
+++ pages/example/CommitLog/index.tsx
@@ -1,10 +1,19 @@
 import { GithubCommitTable } from '@/components/GithubCommitTable';
-import React from 'react';
+import React, { FC } from 'react';
 import dayjs from 'dayjs';
 import { Header } from '@/components/Head';
 import { GlobalContextProvider } from '@/context/GlobalContextProvider';
 
-const CommitLog = () => {
+const CommitLog: FC<any> = ({ commitLogs }) => {
+  return (
+    <GlobalContextProvider>
+      <Header />
+      <GithubCommitTable data={commitLogs} />
+    </GlobalContextProvider>
+  );
+};
+
+export const getServerSideProps = () => {
   const cols = 52;
   const rows = 7;
   const thisWeek = dayjs().day() + 1;
@@ -15,12 +24,8 @@ const CommitLog = () => {
     draft.count = Math.random() * 1000;
     return draft;
   });
-  return (
-    <GlobalContextProvider>
-      <Header />
-      <GithubCommitTable data={commitLogs} />
-    </GlobalContextProvider>
-  );
+  commitLogs.reverse();
+  return { props: { commitLogs } };
 };
 
 export default CommitLog;
