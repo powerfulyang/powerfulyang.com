c56d5ed18219fd356ad6e9e9a4ac33f91b647c02
powerfulyang <powerfulyang@qq.com>
2021-03-18 18:34:44 +0800

    feat: 闪烁的按钮

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,15 @@ const Index: FC = () => {
   return (
     <GlobalContextProvider>
       <Header />
-      <figure className={styles.home_bg} />
+      <figure className={styles.home_bg}>
+        <section className={styles.blog_desc_section}>
+          <div className={styles.blog_title}>萝卜的小窝</div>
+          <div className={styles.blog_desc}>
+            <div className={styles.bio}>一生悬命じゃないかしら</div>
+            <div className={styles.bio}>猜不透的天气 不知何时能放晴</div>
+          </div>
+        </section>
+      </figure>
     </GlobalContextProvider>
   );
 };
