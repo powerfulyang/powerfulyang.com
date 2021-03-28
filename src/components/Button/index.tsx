7f7c5ed3306630868d2cdb946e6451f70f2b6328
powerfulyang <i@powerfulyang.com>
2021-03-28 14:27:18 +0800

    fix: css error

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,13 +5,13 @@ import { Loading } from '@/components/Loading';
 import styles from './index.module.scss';
 
 export const Button: FC<ButtonProps> = (props) => {
-  const { htmlType = 'button', children, className, icon, loading = false, ...rest } = props;
+  const { children, className, icon, loading = false, ...rest } = props;
   return (
-    <button {...rest} type={htmlType} className={classNames(className, styles.button)}>
+    <span {...rest} className={classNames(className, styles.button)}>
       {icon}
       {loading && <Loading />}
       {children}
-    </button>
+    </span>
   );
 };
 
