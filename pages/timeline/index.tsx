3b1fd3699c83ebc7d4d1bc18a29ed47a6a37ebd8
powerfulyang <i@powerfulyang.com>
2021-05-20 21:32:06 +0800

    todo: timeline

--- pages/timeline/index.tsx
+++ pages/timeline/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 const Timeline = () => {
-  return <div>time line</div>;
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.container}>有多少笑容，就有多少泪水。</div>
+      <div className={styles.container}>
+        <input type="text" className="border border-solid border-black" />
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;
