3a948645b27f168549057ada7daba19335540050
powerfulyang <i@powerfulyang.com>
2021-07-28 18:20:24 +0800

    chore: rm log

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -11,7 +11,6 @@ export const request = async (url: string, options: RequestOptions = {}) => {
   const { method = 'GET', ctx } = options;
   const baseUrl = process.env.BASE_URL;
   const headers = pick(['x-real-ip', 'cookie'], ctx?.req.headers || { 'x-real-ip': '127.0.0.1' });
-  console.log(headers);
   return fetch(`${baseUrl}${url}`, {
     method,
     headers,
