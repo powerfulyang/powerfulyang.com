362704377ae5ba2a1d31d860b835889ae23e6fd4
powerfulyang <i@powerfulyang.com>
2021-10-13 10:48:40 +0800

    feat: admin page

--- src/inernal/components/Category/index.tsx
+++ src/inernal/components/Category/index.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import React, { FC } from 'react';
-import styles from '../../index.module.scss';
+import styles from '~/admin/index.module.scss';
 
 type Props = {
   categoryItems: string[];
