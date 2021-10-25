ae1cb4bff0129fdca4be593c88485df0c0c7b222
powerfulyang <i@powerfulyang.com>
2021-10-25 20:18:33 +0800

    chore: code clean

--- pages/funny/random-avatar/index.tsx
+++ pages/funny/random-avatar/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
+import { ReactNiceAvatar } from '@powerfulyang/funny-avatar';
 import styles from './index.module.scss';
 
 const RandomAvatar = () => (
   <main className="bg-[#14161a] h-full">
     <div className={styles.logo}>
-      <Image src="/icons/apple-touch-icon.png" width={50} height={50} className="rounded-xl" />
+      <ReactNiceAvatar />
       <span className="ml-4">funny avatar</span>
     </div>
     <div className={styles.content} />
