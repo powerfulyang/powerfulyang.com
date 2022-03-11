8c5b780832f6b7e458b454d56e8518340612de84
powerfulyang <powerfulyang@qq.com>
2022-03-11 16:34:40 +0800

    feat: @layer component 在 next.js 中无法使用，是能修改 config 文件

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./src/**/*.tsx'],
+  plugins: [
+    plugin(({ addComponents }) => {
+      addComponents({
+        '.pointer': {
+          cursor: `url('/cursor/pointer.ico'), pointer`,
+        },
+        '.default': {
+          cursor: `url('/cursor/default.ico'), default`,
+        },
+      });
+    }),
+  ],
 };
