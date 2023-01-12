7ff2d46a3c449131e4bc0ca280e73be118b70c3c
powerfulyang <powerfulyang@qq.com>
2023-01-12 18:55:03 +0800

    feat: @react-three/fiber

---
+++ src/components/three/Fireflies.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-unknown-property */
+import * as THREE from 'three';
+import { useMemo, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import type FireflyMaterial from '@/shaders/FireflyMaterial';
+import '@/shaders/FireflyMaterial';
+
+export const Fireflies = ({ count = 40 }) => {
+  const shader = useRef<FireflyMaterial>(null!);
+  const [positionArray, scaleArray] = useMemo(() => {
+    const _positionArray = new Float32Array(count * 3);
+    const _scaleArray = new Float32Array(count);
+    for (let i = 0; i < count; i++) {
+      new THREE.Vector3(
+        (Math.random() - 0.5) * 4,
+        Math.random() * 1.5,
+        (Math.random() - 0.5) * 4,
+      ).toArray(_positionArray, i * 3);
+      _scaleArray[i] = Math.random();
+    }
+    return [_positionArray, _scaleArray];
+  }, [count]);
+  useFrame((_, delta) => {
+    shader.current && (shader.current.time += delta / 2);
+  });
+  return (
+    <points key={count}>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={count}
+          array={positionArray}
+          itemSize={3}
+        />
+        <bufferAttribute attach="attributes-aScale" count={count} array={scaleArray} itemSize={1} />
+      </bufferGeometry>
+      <fireflyMaterial ref={shader} transparent depthWrite={false} />
+    </points>
+  );
+};
---
+++ src/pages/funny/index.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unknown-property */
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Fireflies } from '@/components/three/Fireflies';
+
+const App = () => {
+  return (
+    <Canvas
+      style={{
+        height: '100vh',
+      }}
+      dpr={[1, 2]}
+      camera={{ fov: 45, position: [-4, 2, -4] }}
+    >
+      <Suspense fallback={null}>
+        <color attach="background" args={['#1e2243']} />
+        <Fireflies count={50} />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default App;
