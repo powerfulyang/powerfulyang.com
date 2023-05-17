bc4966a1e6e9587825562a3add7a80e64f80aa52
powerfulyang <powerfulyang@qq.com>
2023-05-17 15:52:49 +0800

    build: use sse for video download

--- src/components/MarkdownContainer/plugins/remarkDirectiveHandle.ts
+++ src/components/MarkdownContainer/plugins/remarkDirectiveHandle.ts
@@ -2,10 +2,21 @@ import { visit } from 'unist-util-visit';
 import type { Root } from 'remark-gfm';
 
 /**
+ * @description ::: containerDirective
+ * @description containerDirective 需要在前面加上 ::: 并且以 ::: 结尾，需要在单独一行使用
+ * @description :: leafDirective
+ * @description leafDirective 需要在单独一行使用
+ * @description : textDirective
+ * @description 单独使用会被 p 标签包裹
  * @example
- * youtube: `:::youtube{#rg80JMm-E1I}`
- * codepen: `:::codepen{#rNvWbQq}`
- * codesandbox: `:::codesandbox{#codesandbox-embed}`
+ * :::
+ * This is a container directive
+ * ::This is a leaf directive
+ * :This is a text directive
+ * youtube: `:youtube{#rg80JMm-E1I}`
+ * codepen: `:codepen{#rNvWbQq}`
+ * codesandbox: `:codesandbox{#codesandbox-embed}`
+ * :::
  */
 export function remarkDirectiveHandle() {
   return (tree: Root) => {
@@ -40,8 +51,6 @@ export function remarkDirectiveHandle() {
             width: '100%',
             height: '450px',
             frameBorder: 0,
-            allow: 'picture-in-picture',
-            allowFullScreen: true,
           };
         }
         if (node.name === 'codesandbox') {
@@ -52,8 +61,6 @@ export function remarkDirectiveHandle() {
             width: '100%',
             height: '450px',
             frameBorder: 0,
-            allow: 'picture-in-picture',
-            allowFullScreen: true,
           };
         }
       }
